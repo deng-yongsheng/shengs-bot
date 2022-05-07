0bd36fd3d33cca2a6d84068e9015f29723a65a35
邓永盛 <1596953204@qq.com>
2022-05-07 17:18:31 +0800

    添加班级魔方打卡

---
+++ request/cube.py
@@ -0,0 +1,74 @@
+import enum
+import json
+import re
+import requests
+from bs4 import BeautifulSoup
+
+from db import CubeAutoPunch
+
+
+def generate_session() -> requests.Session:
+    i_sess = requests.session()
+    i_sess.headers.update({
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/93.0.4577.63 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Refer': 'http://www.banjimofang.com/teacher/course/13709/punch/result/397231/recent',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Cookie': 'remember_teacher_59ba36addc2b2f9401580f014c7f58ea4e30989d=36678%7CJK3fy2BryAB0DRWjjGHc'
+                  'MpE7ooT72ZbyEw3QKdC060oCUK1bEUCcmpeIcC2t%7C%242y%2410%24W9oOJsrCW1VX7%2FSXO5Nk9.e6TXnB'
+                  'pP0YxDE%2FomCdHbW%2FAIHWuZzDC; yxktmf=c78o6TdA5lu9khH2Jt4JnDqf6DCdutsNeBDM1HqN'})
+    return i_sess
+
+
+sess = generate_session()
+
+
+def get_cur_punch_url() -> list:
+    """
+    获取当前的所有打卡链接
+    """
+    res = sess.get('https://k8n.cn/teacher/course/13709/punch/')
+    soup = BeautifulSoup(res.text, features='lxml')
+    punch_list = ['https://k8n.cn' + cur_punch['href'] for cur_punch in
+                  soup.select('a.weui-cell_access')]
+    return punch_list
+
+
+def get_unpunched_students(punch_url) -> dict:
+    """
+    获取班级没有打卡学生名单
+    :param punch_url:
+    :return:
+    """
+    res = sess.get(punch_url + '?op=get')
+    if 'data' in res.json():
+        return res.json()['data']
+    else:
+        return res.json()
+
+
+def get_leave(url: str) -> set:
+    """
+    获取请假且离校人员名单
+    """
+    res = sess.get(url)
+    json_str_res = re.search(r'(?<=leaving_students = )\[.*\](?=;)', res.text)
+    if json_str_res:
+        json_str = json_str_res.group()
+        j = json.loads(json_str)
+        ask_and_leave_school = [i['student_id'] for i in j if i['leave_school'] == 1]
+        return set(ask_and_leave_school)
+    else:
+        return set()
+
+
+def set_punch_state(t_punch_url, punch_record: CubeAutoPunch):
+    """
+    修改打卡状态
+    :param t_punch_url: 打卡的链接
+    :param punch_record: 自动打卡记录
+    :return:
+    """
+    sess.get(t_punch_url + f'?op=set&sid={punch_record.student.cube_id}&status={punch_record.cube_punch_state_id}')
+    print(f'将{punch_record.student.student_name} 的打卡状态修改为 {punch_record.cube_punch_state.cube_punch_state_name}')
--- scripts/cube.py
+++ scripts/cube.py
@@ -1,7 +1,10 @@
+import unittest
+
 import request
 import service
 import exceptions
 from message import send_qq_with_at
+import request
 
 
 def alert_classes():
@@ -9,4 +12,46 @@ def alert_classes():
     班级魔方考勤提醒
     :return:
     """
-    pass
+    # 获取打卡列表
+    punch_list = request.cube.get_cur_punch_url()
+    punch_list = [
+        'http://www.banjimofang.com/teacher/course/13709/punch/result/791738/recent'
+    ]
+    if punch_list:
+        print('当前有打卡')
+        for punch_url in punch_list:
+            # 获取请假学生名单
+            qingjia_list = punch_url
+            # 读取没有打卡的学生名单
+            data = request.cube.get_unpunched_students(punch_url)
+            # class_unreport_list = {'18软件工程1班': [],
+            #                        '18软件工程2班': [],
+            #                        '19计科1班': [],
+            #                        '19计科2班': [],
+            #                        '19软工1班': [],
+            #                        '19软工2班': [],
+            #                        '19计科日双': []}
+            for clas in service.get_cube_classes():
+                for stu_cube_id in data['0']:
+                    # 判断不在请假离校名单里
+                    if stu_cube_id['id'] not in qingjia_list:
+                        class_unreport_list[id_to_class(stu_cube_id['group_id'])].append(id_to_name(stu_cube_id['id']))
+
+                for i in class_unreport_list:
+                    if '朱昊' in class_unreport_list[i]:
+                        class_unreport_list[i].remove('朱昊')
+                    if len(class_unreport_list[i]) > 0:
+                        tixing = '请' + '、'.join(class_unreport_list[i]) + '尽快完成晚考勤打卡'
+                        qun_name = class_to_qun_name(i)
+                        print(i, tixing)
+                        # send_qq(qun_name, tixing)
+
+
+    else:
+        print('当前无打卡，退出程序')
+
+
+class Test(unittest.TestCase):
+
+    def test1(self):
+        alert_classes()
