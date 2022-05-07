b9dfbe0bede7c8c94e22c18cc2b63a447d5cb3cb
邓永盛 <1596953204@qq.com>
2022-05-07 17:18:49 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import unittest
 
 import db
+from message import send_private_msg, send_group_msg
+from scripts import one
 import request
 import service
-from models import Clas, Counselor, Finish
+from db import Clas, Counselor, Finish
 
 
 class DBTest(unittest.TestCase):
@@ -17,17 +19,17 @@ class DBTest(unittest.TestCase):
 
     def test2(self):
         # 查询今天打卡没有完成的班级
-        from sqlalchemy import func, and_
+        from sqlalchemy import func
         from datetime import datetime
-        unfinished = db.session.query(Clas, Finish) \
-            .outerjoin(Finish) \
-            .filter(func.to_days(Finish.time) == func.to_days(datetime.now())) \
-            .all()
-        # unfinished = db.session.query(Clas) \
-        #     .filter(Clas.class_id.not_in(db.session.query(Finish.class_id)
-        #                                  .filter(func.to_days(Finish.time) == func.to_days(datetime.now()))
-        #                                  .subquery())
-        #             ).all()
+        # unfinished = db.session.query(Clas, Finish) \
+        #     .outerjoin(Finish) \
+        #     .filter(func.to_days(Finish.time) == func.to_days(datetime.now())) \
+        #     .all()
+        unfinished = db.session.query(Clas) \
+            .filter(Clas.class_id.not_in(db.session.query(Finish.class_id)
+                                         .filter(func.to_days(Finish.time) == func.to_days(datetime.now()))
+                                         .subquery())
+                    ).all()
         print(unfinished)
 
 
@@ -49,16 +51,31 @@ class RequestTest(unittest.TestCase):
         for clas in service.get_class_to_prompt():
             print('*' * 30)
             print(clas.class_name)
-            un_reported = request.get_unreported(clas)
+            un_reported = request.one.get_unreported(clas)
             print(un_reported)
 
     def test2(self):
         for clas in service.get_class_list():
             print('*' * 30)
             print(clas.class_name)
-            un_reported = request.get_unreported(clas)
+            un_reported = request.one.get_unreported(clas)
             student_list = service.convert_numbers_to_students(un_reported)
             print(student_list)
 
     def test3(self):
         print(service.get_finished_class_list())
+
+
+class MessageTest(unittest.TestCase):
+
+    def test1(self):
+        send_private_msg('1596953204', message='来自python')
+
+    def test2(self):
+        send_group_msg('569525430', '[CQ:at,qq=1596953204]')
+
+
+class AlertTest(unittest.TestCase):
+
+    def test1(self):
+        one.alert_classes(debug=True)
