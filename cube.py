f6c3d486568b34d622f50e6f32749bbc31d44f77
邓永盛 <1596953204@qq.com>
2022-05-07 10:40:14 +0800

    将班级魔方提醒合并到项目

---
+++ cube.py
@@ -0,0 +1,12 @@
+import request
+import service
+import exceptions
+from message import send_qq_with_at
+
+
+def alert_classes():
+    """
+    班级魔方考勤提醒
+    :return:
+    """
+    pass
--- main.py
+++ main.py
@@ -1,66 +1,14 @@
 import time
-import traceback
 
-import request
-import service
-import exceptions
-from message import open_all_windows, send_qq_with_at
-
-
-def alert_classes():
-    """
-    进行班级提醒
-    :return:
-    """
-    # 遍历班级
-    for clas in service.get_class_to_prompt():
-        print("*" * 40)
-        try:
-            print(clas.class_name)
-            # 获取未完成打卡成员学号列表
-            unreported_numbers = request.get_unreported(clas=clas)
-            if len(unreported_numbers) > 0:
-                # 将学号列表转学生信息列表
-                unreported_students = service.convert_numbers_to_students(unreported_numbers)
-                # 是否添加 可爱的xx
-                if_cute = '可爱' if len(unreported_numbers) <= 4 else ''
-                mess = '请' + if_cute + '、'.join(map(lambda x: x.student_name, unreported_students)) + "尽快完成小one易健康打卡\n"
-                print(mess.replace('\n', ''))
-                # 发送消息
-                send_qq_with_at(to_who=clas.class_group_name, msg=mess, at_list=unreported_students)
-            else:
-                # 记录班级打卡完成
-                print(f'{clas.class_name} 打卡完毕！')
-                service.class_finished(class_=clas)
-        except exceptions.TokenExpire:
-            # 提醒对应班级token失效
-            mess = f'{clas.class_name}token失效，请班级管理员 {clas.token.admin_name} 重新打开小one易并登录。'
-            print(mess)
-            send_qq_with_at(clas.class_group_name, msg=mess)
-        except Exception as e:
-            print(e)
-            print(traceback.format_exc())
-            continue
-    print('*' * 60)
-    print('以下班级已经完成打卡，今天不再发送消息提醒：')
-    for clas in service.get_finished_class_list():
-        print(clas)
-
-
-def alert_counselors():
-    """
-    对辅导员进行提醒
-    :return:
-    """
-    pass
-
+from message import open_all_windows
+import one
 
 if __name__ == '__main__':
     # 打开所有的qq窗口
     open_all_windows()
-    # 提醒班级
-    alert_classes()
-    # 提醒辅导员
-    alert_counselors()
+    # 提醒班级群小one易打卡
+    one.alert_classes()
+    # 提醒辅导员小one易打卡
+    one.alert_counselors()
     # 延迟退出
     time.sleep(5)
