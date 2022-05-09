ce9c130b0d573fb985cc6616a97e9e8f8bfc9517
邓永盛 <1596953204@qq.com>
2022-05-09 21:53:55 +0800

    Update 邓永盛.py

--- messge_process/scripts/邓永盛.py
+++ messge_process/scripts/邓永盛.py
@@ -2,13 +2,10 @@ from ..bot import specified_private_message_handler
 from ..message_type import PrivateMessageReply, PrivateMessage
 
 
-
 @specified_private_message_handler(1596953204)
 def repeat(message: PrivateMessage):
-    reply = None
     if message.raw_message == 'ping':
         reply = PrivateMessageReply(reply='pong')
     else:
         reply = None
-
     return reply
