f3f4241b66a481f415e4ff885e5dd2cb29f4d304
邓永盛 <1596953204@qq.com>
2022-05-18 19:13:52 +0800

    Create 肖沫晔.py

---
+++ bot_reply/scripts/肖沫晔.py
@@ -0,0 +1,17 @@
+import os
+import threading
+
+from ..bot import certain_private_message_handler
+from message_type import PrivateMessageReply, PrivateMessage
+
+
+@certain_private_message_handler(2694607943)
+def make_response(message: PrivateMessage):
+    if message.raw_message == '打卡情况':
+        reply = PrivateMessageReply('正在查询小one易打卡情况')
+        # 使用os.system 防止数据库长时间连接导致断开
+        thread = threading.Thread(os.system, args=('python main.py one alert counselor',))
+        thread.start()
+    else:
+        reply = None
+    return reply
