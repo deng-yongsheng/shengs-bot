054a37d0590d0335c14a82339e8afd75c303c87f
邓永盛 <1596953204@qq.com>
2022-05-21 21:23:25 +0800

    Update sheng.py

--- bot_reply/scripts/sheng.py
+++ bot_reply/scripts/sheng.py
@@ -9,3 +9,17 @@ def repeat(message: PrivateMessage):
     else:
         reply = None
     return reply
+
+
+@certain_private_message_handler(1596953204)
+def make_response(message: PrivateMessage):
+    import threading
+    import os
+    if message.raw_message == '打卡情况':
+        reply = PrivateMessageReply('正在查询小one易打卡情况')
+        # 使用os.system 防止数据库长时间连接导致断开
+        thread = threading.Thread(target=os.system, args=('python main.py one counselor',))
+        thread.start()
+    else:
+        reply = None
+    return reply
